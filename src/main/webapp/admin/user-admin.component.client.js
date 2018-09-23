c42faa0c64253983abbeb3bac767fcd607c85b8f
Adam Smith <apsmith@ccs.neu.edu>
2018-09-23 16:25:25 -0400

    implemented createUser

--- src/main/webapp/admin/user-admin.component.client.js
+++ src/main/webapp/admin/user-admin.component.client.js
@@ -1,8 +1,8 @@
 (function () {
-    var $usernameFld, $passwordFld; // note: the $ is a clue to where this came from/is mapped to.
+    var $usernameFld, $passwordFld; // note: the $ is a clue that this variable is bound to an element in the DOM.
     var $removeBtn, $selectBtn;
     var $createBtn, $updateBtn, $searchBtn;
-    var $firstNameFld, $lastNameFld;
+    var $firstNameFld, $lastNameFld, $roleFld;
     var $userRowTemplate, $tbody;
     var userService = new AdminUserServiceClient();
     $(main);
@@ -12,15 +12,51 @@
         $passwordFld = $("#passwordFld");
         $firstNameFld = $("#firstNameFld");
         $lastNameFld = $("#lastNameFld");
+        $roleFld = $("#roleFld");
 
+        $userRowTemplate = $(".wbdv-template.wbdv-user");
+        $tbody = $(".wbdv-tbody");
+
+        $searchBtn = $("#searchBtn");
         $createBtn = $("#createBtn");
         $updateBtn = $("#updateBtn");
-        $searchBtn = $("#searchBtn");
 
         $createBtn.click(createUser);
     }
     function createUser() {
-        alert('create user')
+        var username = $usernameFld.val();
+        var password = $passwordFld.val();
+        var firstName = $firstNameFld.val();
+        var lastName = $lastNameFld.val();
+        var role = $roleFld.val();
+        clearform();
+
+        var newUser = $userRowTemplate.clone();
+        newUser
+            .removeClass("wbdv-hidden")
+            .find(".wbdv-username")
+            .html(username);
+        newUser
+            .find(".wbdv-password")
+            .html(password);
+        newUser
+            .find(".wbdv-first-name")
+            .html(firstName);
+        newUser
+            .find(".wbdv-last-name")
+            .html(lastName);
+        newUser
+            .find(".wbdv-role")
+            .html(role);
+
+        $tbody.append(newUser);
+    }
+    function clearform() {
+        $usernameFld.val("");
+        $passwordFld.val("");
+        $firstNameFld.val("");
+        $lastNameFld.val("");
+        $roleFld.val("FACULTY");
     }
     function findAllUsers() {
 
