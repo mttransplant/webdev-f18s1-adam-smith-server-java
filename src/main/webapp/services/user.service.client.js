542b830a70f8ff0dd7b2473da5f1c87aae9f486a
Adam Smith <apsmith@ccs.neu.edu>
2018-09-23 15:14:29 -0400

    start work on findAllUsers()

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -4,11 +4,47 @@ function AdminUserServiceClient() {
     this.findUserById = findUserById;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
-    this.url = 'http://localhost:8080/api/user';
+    // this.url = 'http://localhost:8080/api/user';
     var self = this;
-    function createUser(user, callback) { … }
-    function findAllUsers(callback) { … }
-    function findUserById(userId, callback) { … }
-    function updateUser(userId, user, callback) { … }
-    function deleteUser(userId, callback) { … }
+
+    function createUser(user, callback) {
+
+    }
+    function findAllUsers() {
+        return [
+            {
+                "username": "alice",
+                "password": "alice",
+                "email": "alice@wonderland.com",
+                "firstName": "Alice",
+                "lastName": "Wonderland",
+                "role": "FACULTY"
+            },
+            {
+                "username": "bob",
+                "password": "bob",
+                "email": "bob@builder.com",
+                "firstName": "Bob",
+                "lastName": "Builder",
+                "role": "STUDENT"
+            },
+            {
+                "username": "charly",
+                "password": "charly",
+                "email": "charly@peanuts.com",
+                "firstName": "Charly",
+                "lastName": "Brown",
+                "role": "STUDENT"
+            }
+        ]
+    }
+    function findUserById(userId, callback) {
+
+    }
+    function updateUser(userId, user, callback) {
+
+    }
+    function deleteUser(userId, callback) {
+
+    }
 }
